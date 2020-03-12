b317426a67041cdaae3e4b24983a8d65d6724c0b
drincastX <drincast@gmail.com>
2020-03-12 17:24:21 -0500

    Se inicia dev para seleccionar la canción a cargar

--- src/App.js
+++ src/App.js
@@ -2,12 +2,16 @@ import React, { useState } from 'react';
 import { withStyles } from '@material-ui/core/styles';
 import Box from '@material-ui/core/Box';
 import Container from '@material-ui/core/Container';
+import Select from '@material-ui/core/Select';
+import MenuItem from '@material-ui/core/MenuItem';
 
 import Header from './component/Header';
 import LyricOnly from './component/LyricOnly';
 import StyleTheme from './component/StyleTheme';
 
-import GetSongs from "./TempData";
+import GetSongs from './TempData';
+import DataThemes from './DataThemes.json';
+import DataLyrics from './DataLyrics.json';
 
 const StlMuiContainerPpal = withStyles({
     root:{        
@@ -17,6 +21,10 @@ const StlMuiContainerPpal = withStyles({
 })(Container);
 
 function App() {
+    
+
+    console.log(Object.keys(DataThemes));
+
     const [styleTheme, setStyleTheme] = useState({
         bgcolor: 'white',
         color: 'black'
@@ -52,6 +60,14 @@ function App() {
             <Box bgcolor={styleTheme.bgcolor} color={styleTheme.color} height='100%'>
                 <Header />
                 <StyleTheme changeTheme={changeTheme} />
+                <Select>
+                    <MenuItem value="">
+                        <em>None</em>
+                    </MenuItem>
+                    <MenuItem value='1'>Ten</MenuItem>
+                    <MenuItem value='2'>Twenty</MenuItem>
+                    <MenuItem value='3'>Thirty</MenuItem>
+                </Select>
                 <button onClick={getSong}>song</button>
                 <LyricOnly by={song.by} lyric={song.lyric} title={song.title}/>
             </Box>
